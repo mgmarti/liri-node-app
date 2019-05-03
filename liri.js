19053d24057da92d45a1582985b3cb7f39c7cb43
monica <mogimartinez@gmail.com>
2019-05-03 18:47:20 -0500

    added venue and venue location to searchConcerts function

--- liri.js
+++ liri.js
@@ -13,6 +13,9 @@ const fs = require("fs");
 //---- REQUIRE AXIOS ----
 const axios = require("axios");
 
+//---- REQUIRE MOMENT ----
+const moment = require("moment");
+
 
 //---- INITIALIZE SPOTIFY ----
 const Spotify = require("node-spotify-api")
@@ -29,9 +32,9 @@ let omdb = (keys.omdb);
 const command = process.argv[2];
 const userInput = process.argv[3];
 
-
 //---- LIRI COMMANDS ----
 function startApp(command, userInput) {
+
     switch (command) {
         case "search-songs":
             spotifyThis();
@@ -46,7 +49,7 @@ function startApp(command, userInput) {
             readFile();
             break;
         default:
-            console.log("Try Again");
+            // console.log("\nHi, I'm LIRI. \n\nI can help you find songs, concerts, and movies by typing following commands: \n\nFOR SONG INFO: search-songs 'song name' \nFOR CONCERT INFO: search-concerts 'artist name' \nFOR MOVIE INFO: search-movies 'movie name' \n\nSee what I can do! Type: search-songs 'on a good day'");
     }
 }
 
@@ -111,15 +114,19 @@ function searchConcerts() {
     const concert = process.argv[3];
     // console.log(concert)
 
-    axios.get("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + ticketmaster.id + "&keyword=" + concert)    
+    axios.get("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + ticketmaster.id + "&keyword=" + concert)
         .then(function (response) {
             const concertInfo = response.data._embedded.events;
-            console.log(concertInfo[0].name);
-            console.log(concertInfo[0]._embedded.venues[0].name);
+            const concertDate = response.data._embedded.events[0].dates.start.dateTime;
+            const date = moment(concertDate).format("MM/DD/YYYY");
+            // console.log(concertDate);
+            // console.log(date)
             // console.log(response.data._embedded.events);
-            // console.log("VENUE: ");
-            // console.log("VENUE LOCATION: ");
-            // console.log("DATE: ");
+
+            console.log("ARTIST: " + concertInfo[0].name);
+            console.log("DATE: " + date);
+            console.log("VENUE: " + concertInfo[0]._embedded.venues[0].name);
+            console.log("VENUE LOCATION: " + concertInfo[0]._embedded.venues[0].address.line1 + " " + concertInfo[0]._embedded.venues[0].city.name + ", " + concertInfo[0]._embedded.venues[0].state.stateCode + " " + concertInfo[0]._embedded.venues[0].postalCode);
         })
         .catch(function (error) {
             console.log(error);
